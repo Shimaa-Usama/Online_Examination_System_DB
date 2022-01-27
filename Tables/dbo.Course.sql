0b8f80d17010aa6358342022a8fe36048324f105
reemyasser <47141056+reemyasser@users.noreply.github.com>
2022-01-27 20:24:51 +0200

    reem

--- Tables/dbo.Course.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Course] (
-  [Crs_id] [int] NOT NULL,
-  [Name] [nvarchar](50) NULL,
-  [Duration] [int] NULL,
-  CONSTRAINT [PK_Course] PRIMARY KEY CLUSTERED ([Crs_id])
-)
-ON [PRIMARY]
-GO
