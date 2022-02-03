a9caf4bac577f5cb1f4a8e4353b9c83e79df50ec
ayazaher <princessyoyo9812@gmail.com>
2022-02-03 17:44:16 +0200

    ReportProc & update on insert exam & exam table

--- Programmability/Procedures/dbo.insert_Exam.sql
+++ Programmability/Procedures/dbo.insert_Exam.sql
@@ -1,9 +1,9 @@
 ﻿SET QUOTED_IDENTIFIER, ANSI_NULLS ON
 GO
-create proc [dbo].[insert_Exam] @id int,@grade int, @crs_id int
+CREATE proc [dbo].[insert_Exam] @grade int, @crs_id int
 as
 begin 
-insert into Exam values(@id,@grade,@crs_id)
+insert into Exam values(@grade,@crs_id)
 
 end
 GO
