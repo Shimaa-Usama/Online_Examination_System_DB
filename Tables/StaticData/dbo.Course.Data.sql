6f4b5c2b351adf1ea639c9247d44d32ad589dec8
reemyasser <47141056+reemyasser@users.noreply.github.com>
2022-02-04 12:06:19 +0200

    insert data

---
+++ Tables/StaticData/dbo.Course.Data.sql
@@ -0,0 +1,2 @@
+﻿INSERT dbo.Course(Crs_id, Name, Duration) VALUES (1, N'C#', 60);
+INSERT dbo.Course(Crs_id, Name, Duration) VALUES (2, N'SQL', 90);
---
+++ Tables/StaticData/dbo.Student.Data.sql
@@ -0,0 +1,5 @@
+﻿INSERT dbo.Student(St_id, Fname, Lname, Email, Password, Address, Age, Dept_id) VALUES (1, N'Aya', N'Zaher', N'aya123@gmail.com', N'123', NULL, 23, 10);
+INSERT dbo.Student(St_id, Fname, Lname, Email, Password, Address, Age, Dept_id) VALUES (2, N'Reem', N'Yasser', N'reem123@gmail.com', N'123', NULL, 24, 20);
+INSERT dbo.Student(St_id, Fname, Lname, Email, Password, Address, Age, Dept_id) VALUES (3, N'Shimaa', N'Osama', N'shimaa123@gmail.com', N'123', NULL, 23, 30);
+INSERT dbo.Student(St_id, Fname, Lname, Email, Password, Address, Age, Dept_id) VALUES (4, N'yasmen', N'mkdad', N'yasmen123@gmail.com', N'123', NULL, 22, 40);
+INSERT dbo.Student(St_id, Fname, Lname, Email, Password, Address, Age, Dept_id) VALUES (5, N'yoyo', N'zaher', NULL, NULL, NULL, NULL, 10);
