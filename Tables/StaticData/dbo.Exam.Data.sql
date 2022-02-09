563ce85b24357130eaedc4e39acf68dabbca101e
reemyasser <47141056+reemyasser@users.noreply.github.com>
2022-02-09 18:07:21 +0200

    data exam

--- Tables/StaticData/dbo.Exam.Data.sql
+++ Tables/StaticData/dbo.Exam.Data.sql
@@ -1,8 +1,7 @@
 ﻿SET IDENTITY_INSERT dbo.Exam ON
 GO
-INSERT dbo.Exam(Ex_id, Grade, Crs_id) VALUES (1, 40, 1);
-INSERT dbo.Exam(Ex_id, Grade, Crs_id) VALUES (2, 40, 1);
-INSERT dbo.Exam(Ex_id, Grade, Crs_id) VALUES (3, 40, 1);
+INSERT dbo.Exam(Ex_id, Grade, Crs_id) VALUES (1, 100, 1);
+
 GO
 SET IDENTITY_INSERT dbo.Exam OFF
 GO
